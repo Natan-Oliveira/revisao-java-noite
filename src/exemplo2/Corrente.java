6ed67cd7189d4d9246be078c6ecef69af9beac2f
NATANAEL SENA DE OLIVEIRA <a9245101@rn.senac.br>
2018-07-25 21:17:43 -0300

    Contas

---
+++ src/exemplo2/Corrente.java
@@ -0,0 +1,7 @@
+package exemplo2;
+
+public class Corrente extends Conta {
+	
+	private float limite;
+	private float tarifa;
+}
---
+++ src/exemplo2/Poupança.java
@@ -0,0 +1,6 @@
+package exemplo2;
+
+public class Poupança extends Conta {
+
+	private float rendimento;
+}
